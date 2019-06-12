b17dc9b55652960d82ba7b1308fdfe6ae9a81950
dafyddllyr <dafyddllyr@gmail.com>
2019-06-12 13:51:26 +0100

    MVP complete

---
+++ models/cinema.js
@@ -0,0 +1,29 @@
+const Cinema = function (films) {
+  this.films = films;
+};
+
+Cinema.prototype.filmTitles = function () {
+  return this.films.map(film => film.title);
+};
+
+Cinema.prototype.findByTitle = function (title) {
+  return this.films.filter(film => film.title === title)[0];
+}
+
+Cinema.prototype.filterByGenre = function (genre) {
+  return this.films.filter(film => film.genre === genre);
+}
+
+Cinema.prototype.filmsByYear = function (year) {
+  return this.films.some(film => film.year === year);
+}
+
+Cinema.prototype.allFilmLengthCheck = function (length) {
+  return this.films.every(film => film.length > length)
+}
+
+Cinema.prototype.totalRunningTime = function () {
+  return this.films.reduce((a, b) => a + b.length, 0)
+}
+
+module.exports = Cinema;
